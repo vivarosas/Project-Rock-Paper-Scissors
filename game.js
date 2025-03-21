480659ebdd51c3146eb555a4a2495c1f21811ede
Vivarosas <cersosimotomas772@gmail.com>
2025-03-21 11:30:12 -0300

    crada la funcion para pedir una elccion al usario

--- game.js
+++ game.js
@@ -1,10 +1,20 @@
 // 1. crear una funcion que eliga una de las opciones aleatoriamente.                                          2.una funcion para conseguir una eleccion del usuario    3.crear una variable para el seguimiento del puntaje     4.tiene que poder jugarse 5 rondas//
 
+//PUEDE QUE NECESITE USAR ESTO EN SU DEVIDO MOMENTO "toLowerCase"//
 
-function Randomlista(Array) {
+//esta funcion te da una opcion aleatoria entre las tres posibilidades//
+function getComputerChoice(Array) {
     var indiceAleatorio = Math.floor(Math.random() * Array.length);
     return Array[indiceAleatorio];
 }
 
-var opciones = ["Piedra", "Papel", "Tijera"]
-console.log(Randomlista(opciones))
+var opciones = ["Piedra", "Papel", "Tijera"];
+var contra = getComputerChoice(opciones);
+
+
+//esta funcion le pedira al usuario que elija entre piedra, papel o tijera//
+function getHumanChoice(prompt) {
+    let choise = prompt("Piedra, Papel p Tijera?");
+    return choise
+}
+console.log(getHumanChoice(prompt))
